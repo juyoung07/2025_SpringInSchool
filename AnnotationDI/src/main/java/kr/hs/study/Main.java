5fc69a87fbc3610fc903f51fbe9993f4f0d2ba6c
juyoung07 <yunju5090@gmail.com>
2025-03-21 11:58:17 +0900

    [Annotation] : 참조형 주입

--- AnnotationDI/src/main/java/kr/hs/study/Main.java
+++ AnnotationDI/src/main/java/kr/hs/study/Main.java
@@ -1,6 +1,7 @@
 package kr.hs.study;
 
 import kr.hs.study.beans.TestBean1;
+import kr.hs.study.beans.TestBean2;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -10,9 +11,12 @@ public class Main {
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("config_annotation.xml");
 
         TestBean1 obj = ctx.getBean("t1", TestBean1.class);
-        System.out.println(obj.getData1());
-        System.out.println(obj.getData2());
-        System.out.println(obj.getData3());
+        System.out.println("data1 : " + obj.getData1());
+        System.out.println("data2 : " + obj.getData2());
+        System.out.println("data3 : " + obj.getData3());
+
+        TestBean2 obj2 = ctx.getBean("t2", TestBean2.class);
+        System.out.println("obj2's Data1 : " + obj2.getData1());
 
         ctx.close();
     }
