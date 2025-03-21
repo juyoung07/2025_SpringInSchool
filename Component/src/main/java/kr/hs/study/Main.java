14841f39ca5888de3c6630fb8b2c5015ccdcf10a
juyoung07 <yunju5090@gmail.com>
2025-03-21 11:22:05 +0900

    [Annotation] : 객체 생성!

---
+++ Component/src/main/java/kr/hs/study/Main.java
@@ -0,0 +1,21 @@
+package kr.hs.study;
+
+import kr.hs.study.beans.TestBean1;
+import kr.hs.study.beans.TestBean2;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("config_annotation.xml");
+
+        TestBean1 obj = ctx.getBean("testBean1", TestBean1.class);
+        System.out.println("obj : " + obj);
+
+        TestBean2 obj2 = ctx.getBean("t2", TestBean2.class);
+        System.out.println("obj2 : " + obj2);
+
+        ctx.close();
+    }
+}
